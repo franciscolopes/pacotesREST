f5f3f2ad63465c357564c0450d9c3cfbe63f76a9
Francisco Lopes <franciscolopesdacunha@gmail.com>
2016-11-28 22:43:26 -0200

    Add: Teste de API básica (etapa 8) realizada com sucesso

--- src/main/java/com/franciscolopes/pacotes/recursos/ClienteRecurso.java
+++ src/main/java/com/franciscolopes/pacotes/recursos/ClienteRecurso.java
@@ -1,14 +1,68 @@
 package com.franciscolopes.pacotes.recursos;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.franciscolopes.pacotes.dominio.Cliente;
+import com.franciscolopes.pacotes.repositorio.ClienteRepositorio;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteRecurso {
+	
+	@Autowired
+	private ClienteRepositorio repo;
+
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String todos() {
-		return "Executou o metodo todos";
+	public ResponseEntity<List<Cliente>> todos() {
+		return ResponseEntity.status(HttpStatus.OK).body(repo.findAll());
 	}
+
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscar(@PathVariable("id") Integer id) {
+		Cliente cli = repo.findOne(id);
+		if (cli == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(cli);
+	}
+
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> criar(@RequestBody Cliente cliente) {
+		cliente = repo.save(cliente);
+		URI uri = getUri("/{id}", cliente.getCodCliente());
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deletar(@PathVariable("id") Integer id) {
+		repo.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> atualizar(@RequestBody Cliente cliente, @PathVariable("id") Integer id) {
+		cliente.setCodCliente(id);
+		repo.save(cliente);
+		return ResponseEntity.noContent().build();
+	}
+	
+	private URI getUri(String nome, Integer valor) {
+		return ServletUriComponentsBuilder.fromCurrentRequest().path(nome).buildAndExpand(valor).toUri();
+	}
+
+
 }
